c0a6ce8715fcfa01dd7412cdf7d910e42689b5a5
VPDong <vpdong@qq.com>
2020-05-06 17:34:20 +0800

    init

--- app/src/main/java/com/vpdong/lib/test/view/MainActivity.java
+++ app/src/main/java/com/vpdong/lib/test/view/MainActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import com.vpdong.lib.java.android.mvp.view.MVPActivity;
 import com.vpdong.lib.test.R;
 
-public class MainActivity extends MVPActivity {
+public class MainActivity extends MVPActivity<MainPresenter> {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
---
+++ app/src/main/java/com/vpdong/lib/test/view/MainPresenter.java
@@ -0,0 +1,7 @@
+package com.vpdong.lib.test.view;
+
+import com.vpdong.lib.java.android.mvp.MVPPresenter;
+
+public class MainPresenter extends MVPPresenter<MainActivity> {
+	// todo
+}
