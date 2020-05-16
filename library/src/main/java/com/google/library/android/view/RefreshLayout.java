9c0545d7aceba5d872b76d1b98edf6d76b6dc9e4
VPDong <vpdong@qq.com>
2020-05-17 00:00:03 +0800

    init

--- library/src/main/java/com/google/library/android/view/RefreshLayout.java
+++ library/src/main/java/com/google/library/android/view/RefreshLayout.java
@@ -2,16 +2,14 @@ package com.google.library.android.view;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 public class RefreshLayout extends SwipeRefreshLayout {
-	public RefreshLayout(@NonNull Context context) {
+	public RefreshLayout(Context context) {
 		this(context, null);
 	}
 	
-	public RefreshLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
+	public RefreshLayout(Context context, AttributeSet attrs) {
 		super(context, attrs);
 	}
 }
